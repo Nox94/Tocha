1c7c39fd5c8387b38131acef13c176c8069b23bd
Евгения <69312077+Nox94@users.noreply.github.com>
2021-08-30 17:24:42 +0300

    создает компонент формы

---
+++ src/components/Form/Form.js
@@ -0,0 +1,12 @@
+export default function Form() {
+    return (
+        <form className="form">
+            <h2 className="form__heading">Регистрация</h2>
+            <input className="form__input"/>
+            <input className="form__input"/>
+            <input className="form__input"/>
+            <input className="form__input"/>
+            <button className="form__button">Зарегистрироваться</button>
+        </form>
+    )
+}
