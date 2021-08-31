1dfc0950bb3ba5ceb086c25774d6ee1a3c9b636a
Евгения <69312077+Nox94@users.noreply.github.com>
2021-08-31 16:48:25 +0300

    подключает библиотеку react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Form from "../Form/Form.js";
+import {Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-<Form/>
+        <Switch>
+            <Route path={["/signup", "/signin"]} component={Form}/>
+        </Switch>
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,23 @@
+import 'form.scss';
+import {Route} from "react-router-dom";
+
 export default function Form() {
     return (
         <form className="form">
-            <h2 className="form__heading">Регистрация</h2>
-            <input className="form__input"/>
-            <input className="form__input"/>
-            <input className="form__input"/>
-            <input className="form__input"/>
-            <button className="form__button">Зарегистрироваться</button>
+            <Route path={"/signup"}>
+                <h2 className="form__heading">Регистрация</h2>
+                <input className="form__input"/>
+                <input className="form__input"/>
+                <input className="form__input"/>
+                <input className="form__input"/>
+                <button className="form__button">Зарегистрироваться</button>
+            </Route>
+            <Route path={"/signin"}>
+                <h2 className="form__heading">Вход</h2>
+                <input className="form__input"/>
+                <input className="form__input"/>
+                <button className="form__button">Войти</button>
+            </Route>
         </form>
     )
 }
